9eac048a7f45bf5a99516f30e9690cb56ed07615
redaktor <mail@redaktor.me>
2020-12-21 17:14:56 +0100

    only terrain test

---
+++ tiles/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="terrain" tilewidth="32" tileheight="32" tilecount="630" columns="30">
+ <image source="terrain.png" width="960" height="672"/>
+</tileset>
